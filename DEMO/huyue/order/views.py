67561cf48372926c7d69a9e966270162e8a99cde
chenzhuo <chenzhuo_249@163.com>
2020-04-12 22:17:22 +0800

    add demo

--- DEMO/huyue/order/views.py
+++ DEMO/huyue/order/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Order
 # Create your views here.
+
 def order_view(request):
     # return render(request, "order/order.html")
-    Order.objects.create(order_number="201903220002", order_time="2019/03/22", name="良品铺子", price=89.00, count=1, img_path="g_02.jpg",  total=89, status=2)
-    res = Order.objects.all()
-    return HttpResponse(res[0].name)
+    # Order.objects.create(order_number="201903220002", order_time="2019/03/22", name="良品铺子", price=89.00, count=1, img_path="g_02.jpg",  total=89, status=2)
+    # res = Order.objects.all()
+    # return HttpResponse(res[0].name)
+
+    return render(request, "order/order.html")
